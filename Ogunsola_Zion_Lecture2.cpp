d115362eac1a4c3e9df7144498aa435d09b6eb8b
ZionOgunsola <ziono12@nycstudents.net>
2024-09-16 22:21:11 -0400

    Create Ogunsola_Zion_Lecture2.cpp
    Lecture 2

---
+++ Ogunsola_Zion_Lecture2.cpp
@@ -0,0 +1,32 @@
+//Zion Ogunsola
+//Lecture 2 
+//9/16/2024
+
+#include <iostream> //Includes iostreams
+
+#include <string> //includes string
+
+int main() //Begins code
+{
+int Ainteger = 4 <<endl; //Defines int as variable Ainteger which is equal to 4
+float Afloatdecimal = 4.5 <<endl; //Defines float as variable Afloatdecimal which is equal to 4.5
+double Adoubledecimal = 4.50 <<endl; //Defines double as variable Adoubledecimal which is equal to 4.50
+char Acharacter = $ <<endl;  //Defines char as variable Acharacter which is equal to $
+bool myBoolean = true <<endl; //Defines bool as variable myBoolean  which is equal to true
+string Astring = "Data Sets" <<endl; //Defines string as variable Astring which is equal to Data Sets
+
+cout <<"int" << Ainteger <<endl; //Outputs int and int value
+cout <<"float" << Afloatdecimal <<endl; //Outputs float and float value
+cout <<"double" << Adoubledecimal <<endl; //Outputs double and double value
+cout <<"char" << Acharacter <<endl; //Outputs char and char value
+cout <<"bool" << myBoolean <<endl; ///Outputs bool and bool value
+cout <<"string" << Astring <<endl; //Outputs string and string value
+
+
+cout <<"The ASCII value for the letter A is less than that for the letter B. The value for the Letter A is 65, and the value for the letter B is 66"<< endl; //Outputs staement
+
+cout <<"2+3.5" <<2+3.5 <<endl; //Outputs expression and value of expression
+cout <<"6 / 4 + 3.9" <<6/4+39 <<endl; //Outputs expression and value of expression
+cout <<"5.4 * 2 - (13.6 + 18 / 2)" <<5.4 * 2 - (13.6 + 18 / 2) <<endl;//Outputs expression and value of expression
+
+    return 0; //Returns function to beginning
